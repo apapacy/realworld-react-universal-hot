88daca208b47bee73e0aa891c12a0a2f4bd2161c
andrey <ovcharenkoav@meta.ua>
2018-09-28 13:49:29 +0300

    fix empty pathame

--- src/react/components/navItem.js
+++ src/react/components/navItem.js
@@ -4,7 +4,7 @@ import Link from '../asyncLink';
 
 const NavItem = ({ children, to, location }) => ( // eslint-disable-line react/prop-types
   <li className="nav-item" key={to}>
-    <Link className={`nav-link${location.pathname.match(new RegExp(`^${to}(/page/[0-9]+)?$`)) ? ' active' : ''}`} to={to}>{children}</Link>
+    <Link className={`nav-link${location.pathname.match(new RegExp(`^${to}(/?page/[0-9]+)?$`)) ? ' active' : ''}`} to={to}>{children}</Link>
   </li>
 );
 
